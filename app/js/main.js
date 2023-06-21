c63d9c3cb54d18a37d505bb5ae00e9b457990d7b
Nickolay Stepanov <niskoflywork@gmail.com>
2023-06-21 17:39:33 +0300

    таблицы на главной

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ $(function () {
 
     //Слайдер с матчами
 
-    let swiper = new Swiper('.matches-slider', {
+    let matches = new Swiper('.matches-slider', {
         slidesPerView: 6,
         spaceBetween: 20,
         infinity: true,
@@ -16,5 +16,40 @@ $(function () {
             prevEl: '.matches-prev',
         },
     });
+
+    //Слайдер для статистики игроков на главной странице
+
+    let players = new Swiper('.players-slider', {
+        keyboard: true,
+        navigation: {
+            nextEl: '.players-next',
+            prevEl: '.players-prev',
+        },
+    });
+
+    //Табы для турнирной таблицы для главной страницы
+
+    let tabs = document.querySelectorAll('.tab-tour-tab')
+    let tabContent = document.querySelectorAll('.tabulation-tour__group')
+
+    tabs.forEach((tab, i) => {
+        tab.addEventListener('click', function (){
+            if(tab.classList.contains('active')){
+                tab.classList.remove('active')
+            }
+            tabs.forEach((elem) => {
+                elem.classList.remove('active')
+            })
+            tab.classList.add('active')
+
+            if (tabContent[i].classList.contains('active')){
+                tabContent[i].classList.remove('active')
+            }
+            tabContent.forEach((elem) => {
+                elem.classList.remove('active')
+            })
+            tabContent[i].classList.add('active')
+        })
+    })
 
 })
